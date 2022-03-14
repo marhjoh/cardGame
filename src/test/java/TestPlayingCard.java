e7fad39f95b4967ddceb88572fe553693ec8552e
Martin Hegnum Johannessen <marhjoh@stud.ntnu.no>
2022-03-14 13:36:06 +0100

    Made a test class for PlayingCard

---
+++ src/test/java/TestPlayingCard.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestPlayingCard {
+
+    private PlayingCard playingCard = new PlayingCard('C', 3);
+
+    @Test
+    void testPlayingCardConstructor(){
+        assertEquals('C', playingCard.getSuit());
+        assertEquals(3, playingCard.getFace());
+    }
+
+    @Test
+    void testGetAsString() {
+        assertEquals("C3", playingCard.getAsString());
+    }
+
+    @Test
+    void testGetSuit() {
+        assertEquals('C', playingCard.getSuit());
+    }
+
+    @Test
+    void testGetFace() {
+        assertEquals(3, playingCard.getFace());
+    }
+}
