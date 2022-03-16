26080668fd828b685c138a8e953ce2bb07791c6a
Martin Hegnum Johannessen <marhjoh@stud.ntnu.no>
2022-03-16 15:54:58 +0100

    Updated javaDoc and added tests to the classes. Also added a getHand method in Hand

--- src/main/java/no/ntnu/idatg/Hand.java
+++ src/main/java/no/ntnu/idatg/Hand.java
@@ -2,6 +2,8 @@ package no.ntnu.idatg;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Represents a hand of cards. The number of cards on hand can differ.
@@ -13,14 +15,13 @@ public class Hand {
 
     private List<PlayingCard> cardsOnHand;
     private DeckOfCards deckOfCards;
-    private List<Character> suits;
-    private Hand hand;
 
     /**
      * Creates an instance of a hand which is held in an ArrayList
      */
     public Hand(){
-        cardsOnHand = new ArrayList<>(deckOfCards.dealHand(5));
+        deckOfCards = new DeckOfCards();
+        cardsOnHand = deckOfCards.dealHand(5);
     }
 
     /**
@@ -28,18 +29,51 @@ public class Hand {
      *
      * @return the whether the hand contains a flush or not as a boolean
      */
-    public boolean checkFlush(){
-        boolean isFlush = false;
-        int cardCounter = 0;
-        for(Character suit : deckOfCards.getAllSuits()){
-            //lambda feil, må telle opp antall av unik char for å sjekke om den er 5 eller mer.
-                   if(cardsOnHand.stream().collect(suit -> suits).count(cardCounter<= 5)){
-                isFlush = true;
+
+    public boolean checkFlushWithMapValues(){
+        Map<Character, Long> map = cardsOnHand.stream()
+                .collect(Collectors.groupingBy(PlayingCard::getSuit, Collectors.counting()));
+
+            for(Long suit : map.values()){
+                if(suit >= 5){
+                    return true;
                 }
             }
-        return isFlush; }
+            return false;
+        }
 
+    /**
+     * Returns whether the hand contains a flush or not.
+     *
+     * @return whether the hand contains a flush or not (5 or more cards of the similar suit) as a boolean
+     */
+    public boolean checkFlushWithMapKeySet(){
+        Map<Character, Long> map = cardsOnHand.stream()
+                .collect(Collectors.groupingBy(PlayingCard::getSuit, Collectors.counting()));
 
+        for(Character suit : map.keySet()){
+            if(map.get(suit) >= 5){
+                System.out.println(suit);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * This method adds a playingCard to the current hand
+     *
+     * @param playingCard the card to be added to the hand
+     */
+    public void addPlayingCardToHand(PlayingCard playingCard){
+        cardsOnHand.add(playingCard);
+    }
+
+    /**
+     * This method sums up the value of all the cards on the hand
+     *
+     * @return the sum as an int
+     */
     public int getIntValueFromCardsOnHand() {
         int sum = 0;
         for (PlayingCard card : cardsOnHand) {
@@ -47,11 +81,37 @@ public class Hand {
         }
         return sum;
     }
-}
 
+    /**
+     * Returns all the heart cards on the hand
+     *
+     * @return the heart cards on the hand as an ArrayList
+     */
+    public ArrayList<PlayingCard> getAllHeartCardsOnHand() {
+        ArrayList<PlayingCard> allHeartCardsOnHand = new ArrayList<>();
+        for (PlayingCard card : cardsOnHand) {
+            char suit = card.getSuit();
+            if (card.getSuit() == 'H') {
+                allHeartCardsOnHand.add(card);
+            }
+        }
+        return allHeartCardsOnHand;
+    }
 
-        //Da kan man for eksempel legge til metoder på denne klassen for å f.eks. sjekke for flush og andre poenggivende kombinasjoner.
+        /*
+    public String getAllHeartCardsAsString() {
+            allHearCardsOnHandString = allHeartCardsOnHand.stream().map(PlayingCard::toString)
+                    .collect(Collectors.joining(", "));
+        }
+        return allHearCardsOnHandString;
+    }*/
 
-
-
+    /**
+     * Returns the size of the hand
+     *
+     * @return the size of the hand as an int
+     */
+    public int getHandSize(){
+        return cardsOnHand.size();
+    }
 }
--- src/test/java/no/ntnu/idatg/TestHand.java
+++ src/test/java/no/ntnu/idatg/TestHand.java
@@ -1,33 +1,64 @@
 package no.ntnu.idatg;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestHand {
+class TestHand {
 
-    private DeckOfCards deckOfCards;
-    private List<PlayingCard> playingCards;
     private Hand hand;
 
     @BeforeEach
-    void setup(){
-        deckOfCards = new DeckOfCards();
-        playingCards = new ArrayList<>();
-        hand = new Hand(playingCards);
+    @DisplayName("This method will create an object for the tests before each test")
+    void setup() {
+        hand = new Hand();
     }
 
     @Test
-    void testConstructor(){
+    @DisplayName("This method test the handConstructor")
+    void testConstructor() {
+        assertEquals(5, hand.getHandSize());
     }
 
     @Test
-    void checkFlush() {
-        deckOfCards.dealHand(5);
-        assertEquals(false, hand.checkFlush());
+    @DisplayName("This test checks if the hand has a flush or not with mapValues, most likely it has not")
+    void testCheckFlushWithMapValues() {
+        assertFalse(hand.checkFlushWithMapValues());
+    }
+
+    @Test
+    @DisplayName("This test checks if the hand has a flush or not with mapKeySet, most likely it has not")
+    void testCheckFlushWithKeySet() {
+        assertFalse(hand.checkFlushWithMapKeySet());
+    }
+
+    @Test
+    @DisplayName("This method tests to add a playingCard to the current hand")
+    void testAddPlayingCardToHand() {
+        PlayingCard playingCard = new PlayingCard('C', 4);
+
+        hand.addPlayingCardToHand(playingCard);
+
+        assertEquals(6, hand.getHandSize());
+    }
+
+    @Test
+    @DisplayName("This method tests the getHandSize method")
+    void testShowAllHeartsOnHand() {
+        assertEquals(5, hand.getHandSize());
+    }
+
+    @Test
+    @DisplayName("This method tests the amount of heart cards on hand, this will never be above 6")
+    void testGetAllHeartCardsOnHand(){
+        assertNotEquals(6, hand.getAllHeartCardsOnHand().size());
+    }
+
+    @Test
+    @DisplayName("This method tests the getHandSize method")
+    void testGetHandSize(){
+        assertEquals(5, hand.getHandSize());
     }
 }
