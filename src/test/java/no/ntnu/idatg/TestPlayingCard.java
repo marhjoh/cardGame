d6f4f62482c472851738e6826233e286a9f253a1
Martin Hegnum Johannessen <marhjoh@stud.ntnu.no>
2022-03-16 14:59:37 +0100

    Added some javadoc in TestPlayingCard

--- src/test/java/no/ntnu/idatg/TestPlayingCard.java
+++ src/test/java/no/ntnu/idatg/TestPlayingCard.java
@@ -2,6 +2,7 @@ package no.ntnu.idatg;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,22 +12,26 @@ class TestPlayingCard {
     private PlayingCard playingCard = new PlayingCard('C', 3);
 
     @Test
+    @DisplayName("This test if the constructor works properly")
     void testPlayingCardConstructor(){
         assertEquals('C', playingCard.getSuit());
         assertEquals(3, playingCard.getFace());
     }
 
     @Test
+    @DisplayName("This test if the playingCard is returned as intended")
     void testGetAsString() {
         assertEquals("C3", playingCard.getAsString());
     }
 
     @Test
+    @DisplayName("This method tests the getSuit method")
     void testGetSuit() {
         assertEquals('C', playingCard.getSuit());
     }
 
     @Test
+    @DisplayName("This method tests the getFace method")
     void testGetFace() {
         assertEquals(3, playingCard.getFace());
     }
