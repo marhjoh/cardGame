ad5b385db2232e826cbe631d06b0a145e1c419dc
Martin Hegnum Johannessen <marhjoh@stud.ntnu.no>
2022-03-21 14:35:09 +0100

    Refactored the pictures

--- src/main/java/no/ntnu/idatg/CardGameApplication.java
+++ src/main/java/no/ntnu/idatg/CardGameApplication.java
@@ -10,9 +10,8 @@ import java.io.IOException;
 public class CardGameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CardGameApplication.class.getResource("cardGame-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(CardGameApplication.class.getResource("/cardGame-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/no/ntnu/idatg/CardGameController.java
+++ src/main/java/no/ntnu/idatg/CardGameController.java
@@ -1,14 +1,51 @@
 package no.ntnu.idatg;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class CardGameController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardGameController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView card1;
+
+    @FXML
+    private ImageView card2;
+
+    @FXML
+    private ImageView card3;
+
+    @FXML
+    private ImageView card4;
+
+    @FXML
+    private ImageView card5;
+
+    @FXML
+    private Button dealHandButton;
+
+    @FXML
+    private Button checkHandButton;
+
+    @FXML
+    private void onDealHandButtonClick(ActionEvent event){
+
+    }
+
+    @FXML
+    private void onCheckHandButtonClick(){
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Hand hand = new Hand();
     }
 }
